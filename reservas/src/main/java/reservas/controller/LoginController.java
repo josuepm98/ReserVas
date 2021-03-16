b5ef9acaf058cf553dbe2e639ecca6f9a5e6878d
Josué Perea Martínez <josuepm98@gmail.com>
2021-03-16 19:01:48 +0100

    CORS en backend

--- reservas/src/main/java/reservas/controller/LoginController.java
+++ reservas/src/main/java/reservas/controller/LoginController.java
@@ -13,11 +13,17 @@ import reservas.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
 
-@Controller
+@CrossOrigin(origins = "http://localhost:19006", methods= {RequestMethod.GET,RequestMethod.POST})
+@RestController
 public class LoginController {
 
     @Autowired
