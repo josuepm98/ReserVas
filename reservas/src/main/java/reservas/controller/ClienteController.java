3252964f4de69d024db28e30e77c207eaefea9f6
Josué Perea Martínez <josuepm98@gmail.com>
2021-03-31 20:20:50 +0200

    model Servicio actualizado

--- reservas/src/main/java/reservas/controller/ClienteController.java
+++ reservas/src/main/java/reservas/controller/ClienteController.java
@@ -8,13 +8,17 @@ import org.springframework.web.bind.annotation.*;
 import reservas.authentication.ManagerUserSession;
 import reservas.model.Cliente;
 import reservas.model.Empresa;
+import reservas.model.Servicio;
 import reservas.model.Usuario;
 import reservas.service.ClienteService;
 import reservas.service.EmpresaService;
+import reservas.service.ServicioService;
 import reservas.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,4 +33,24 @@ import javax.servlet.http.HttpSession;
 @CrossOrigin(origins = "http://localhost:19006", methods= {RequestMethod.GET,RequestMethod.POST})
 @RestController
 public class ClienteController {
+
+    @Autowired
+    ServicioService servicioService;
+
+    @Autowired
+    ServicioService usuarioService;
+
+    @Autowired
+    ManagerUserSession managerUserSesion;
+
+    @GetMapping("/reservas/{nombreUser}")
+    public ResponseEntity<?> reservasUsuario(@PathVariable(value="nombreUser") String nombreUser, Model model, HttpSession session) {
+        managerUserSesion.comprobarUsuarioLogeado(session, nombreUser);
+        //List<Servicio> servicesUser = servicioService.getServiciosUser();
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+
+
 }
