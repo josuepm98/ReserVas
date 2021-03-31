436eb6491ff467228dc099075f2c5b45e050f0e4
Josué Perea Martínez <josuepm98@gmail.com>
2021-03-31 12:56:54 +0200

    controller-service carburando

--- reservas/src/main/java/reservas/controller/ServicioController.java
+++ reservas/src/main/java/reservas/controller/ServicioController.java
@@ -9,12 +9,17 @@ import reservas.authentication.ManagerUserSession;
 import reservas.model.Cliente;
 import reservas.model.Empresa;
 import reservas.model.Usuario;
+import reservas.model.Servicio;
 import reservas.service.ClienteService;
 import reservas.service.EmpresaService;
+import reservas.service.ServicioService;
 import reservas.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,8 +30,31 @@ import sun.security.util.SecurityConstants;
 
 
 import javax.servlet.http.HttpSession;
+import javax.transaction.Transactional;
 
 @CrossOrigin(origins = "http://localhost:19006", methods= {RequestMethod.GET,RequestMethod.POST})
 @RestController
 public class ServicioController {
+
+    @Autowired
+    ServicioService servicioService;
+
+    @Autowired
+    ServicioService usuarioService;
+
+    @Autowired
+    ManagerUserSession managerUserSesion;
+
+    @GetMapping("/servicios") //NO SE LA RUTA QUE PEDIRAN, HOME SEGURAMENTE
+    public ResponseEntity<?> servicios(@RequestBody Usuario usuario, Model model, HttpSession session) {
+        managerUserSesion.comprobarUsuarioLogeado(session, usuario.getNombreUser());
+
+        //List<Servicio> services = servicioService.getServicios(); TENEMOS QUE DEVOLVER LA SELECT DE SERVICIOS Y PASARLA AL FRONT COMO JSON
+
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+
+
+
 }
--- reservas/src/main/java/reservas/service/ServicioService.java
+++ reservas/src/main/java/reservas/service/ServicioService.java
@@ -7,5 +7,34 @@ import reservas.model.Servicio;
 
 import java.sql.*;
 
+@Service
 public class ServicioService {
+
+    private ConexionMySQL SQL = new ConexionMySQL();
+
+    public boolean getServicios(){
+        Connection conn = SQL.conectarMySQL();  // Nos conectamos a la BBDD
+        boolean resultado = false;
+
+        try {
+
+
+            return resultado;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Se ha producido un error.");
+            return resultado;
+        } finally {
+            try {
+                if(conn != null){
+                    conn.close();
+                }
+            } catch (SQLException e){
+                System.out.println(e.getMessage());
+            }
+        }
+    }
+
+
 }
