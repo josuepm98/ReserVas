41211f764ca10683d73915e9b9a3726c73773a90
jappp <jappppack@gmail.com>
2021-04-21 14:56:30 +0200

    cosas de token que se ven mejor

---
+++ reservas/src/main/java/reservas/controller/UsuarioController.java
@@ -0,0 +1,48 @@
+package reservas.controller;
+import java.util.Date;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import reservas.model.Usuario;
+
+@RestController
+public class UsuarioController {
+    @PostMapping("usuario")
+    public Usuario login (@RequestParam("usuario")String nombre, @RequestParam("password")String psw)
+    {
+        String token = getJWTToken(nombre);
+        Usuario user =new Usuario();
+        user.setNombre(nombre);
+        user.setToken(token);
+        return user;
+    }
+   // @org.jetbrains.annotations.NotNull
+    private String getJWTToken(String username) {
+        String secretKey = "mySecretKey";
+        List<GrantedAuthority> grantedAuthorities = AuthorityUtils
+                .commaSeparatedStringToAuthorityList("ROLE_USER");
+
+        String token = Jwts
+                .builder()
+                .setId("softtekJWT")
+                .setSubject(username)
+                .claim("authorities",
+                        grantedAuthorities.stream()
+                                .map(GrantedAuthority::getAuthority)
+                                .collect(Collectors.toList()))
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 600000))
+                .signWith(SignatureAlgorithm.HS512,
+                        secretKey.getBytes()).compact();
+
+        return "Bearer " + token;
+    }
+}
